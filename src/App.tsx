ebd69629fd636f81908506e8b760203eaa29046e
sijinyu <sijinyudev@gmail.com>
2022-09-22 15:46:25 +0900

    feat: 초기 폴더 구조, 절대경로 설정, 하단 앱바 스타일 구현

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BottomTabBar from "./components/bottomTabBar";
+
+function App() {
+  return (
+    <div className="App">
+      <BottomTabBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/bottomTabBar/BottomTabBar.tsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+const BottomTabBar = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  border-top: 1px solid #dbdbdb;
+  height: 52px;
+  z-index: 40;
+`;
+export default BottomTabBar;
